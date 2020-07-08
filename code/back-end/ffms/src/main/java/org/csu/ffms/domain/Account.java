9b2840699e7065b80e1186789fe34057fa63e9ea
Starplatinum977 <974420962@qq.com>
2020-07-08 09:56:17 +0800

    用户模块除了Controller部分

--- code/back-end/ffms/src/main/java/org/csu/ffms/domain/Account.java
+++ code/back-end/ffms/src/main/java/org/csu/ffms/domain/Account.java
@@ -1,4 +1,58 @@
 package org.csu.ffms.domain;
 
 public class Account {
+    private String userid;
+    private String password;
+    private String username;
+    private String email;
+    private String phone;
+    private String familyid;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFamilyid() {
+        return familyid;
+    }
+
+    public void setFamilyid(String familyid) {
+        this.familyid = familyid;
+    }
 }
--- code/back-end/ffms/src/main/java/org/csu/ffms/persistence/AccountMapper.java
+++ code/back-end/ffms/src/main/java/org/csu/ffms/persistence/AccountMapper.java
@@ -2,7 +2,31 @@ package org.csu.ffms.persistence;
 
 import org.csu.ffms.domain.Account;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface AccountMapper {
+    Account getAccountByUserid(String userid);
+
+    Account getAccountByUseridAndPassword(Account account);
+
+    void insertAccount(Account account);
+
+    void insertRelation(Account account);
+
+    void insertSignon(Account account);
+
+    void updateAccount(Account account);
+
+    void updateRelation(Account account);
+
+    void updateSignon(Account account);
+
+    List<Account> getAllAccount();
+
+    void deleteAccount(String userid);
+
+    void deleteRelation(String userid);
+
+    void deleteSignon(String userid);
 }
