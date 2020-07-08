5b5d319e638174e02bd0d3ef3b2cf1b6055d7dd4
liupei <1194897193@qq.com>
2020-07-08 15:43:36 +0800

    支出管理模块初步部分

---
+++ code/back-end/ffms/src/main/java/org/csu/ffms/persistence/IncomeMapper.java
@@ -0,0 +1,15 @@
+package org.csu.ffms.persistence;
+
+
+import org.csu.ffms.domain.Income;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IncomeMapper {
+    void newIncome(Income income);
+    void deleteIncome(int incomeId);
+    void updateIncome(Income income);
+    List<Income> findIncomeList(Income income);
+}
