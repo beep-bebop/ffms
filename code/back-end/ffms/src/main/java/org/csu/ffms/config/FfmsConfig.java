268a4ae096727bca48e60cf55d3f208bbe45a993
lzh <1334201904@qq.com>
2020-07-09 18:13:47 +0800

    finish token

--- code/back-end/ffms/src/main/java/org/csu/ffms/config/FfmsConfig.java
+++ code/back-end/ffms/src/main/java/org/csu/ffms/config/FfmsConfig.java
@@ -2,6 +2,7 @@ package org.csu.ffms.config;
 
 import org.csu.ffms.jwt.handler.FfmsLoginAuthenticationInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @创建时间 2020/7/9
  * @描述
  **/
-@Component
+@Configuration
 public class FfmsConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- code/back-end/ffms/src/main/java/org/csu/ffms/jwt/token/TokenUtil.java
+++ code/back-end/ffms/src/main/java/org/csu/ffms/jwt/token/TokenUtil.java
@@ -1,5 +1,6 @@
 package org.csu.ffms.jwt.token;
 
+import com.alibaba.fastjson.JSONObject;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.csu.ffms.domain.Account;
@@ -11,10 +12,20 @@ import org.csu.ffms.domain.Account;
  **/
 public class TokenUtil {
 
+    //jwt生成token
     public static String getToken(Account account){
         String token="";
-        token= JWT.create().withAudience(account.getUserid())
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userid",account.getUserid());
+        //过期时间
+        jsonObject.put("exp",System.currentTimeMillis()+1000*600);
+        //签发时间
+        jsonObject.put("iat",System.currentTimeMillis());
+        String json = JSONObject.toJSONString(jsonObject);
+        token= JWT.create().withAudience(json)
                 .sign(Algorithm.HMAC256(account.getPassword()));
         return token;
     }
+
+
 }
